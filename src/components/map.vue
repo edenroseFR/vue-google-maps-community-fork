5981bec41d727d8f82d64e92b019529867b5a8b1
Eden Rose Risma <edenroserisma13@gmail.com>
2024-03-25 11:05:48 +0800

    remove inner contents

--- src/components/map.vue
+++ src/components/map.vue
@@ -1,10 +1,5 @@
 <template>
   <div class="vue-map-container" :class="$attrs.class">
-    <div ref="vue-map" class="vue-map"></div>
-    <div class="vue-map-hidden">
-      <slot></slot>
-    </div>
-    <slot name="visible"></slot>
   </div>
 </template>
 
