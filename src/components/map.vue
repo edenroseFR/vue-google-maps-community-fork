c6329d3a6547fbc8ef306f6e8427e31a751fd58a
Eden Rose Risma <edenroserisma13@gmail.com>
2024-03-25 12:43:22 +0800

    add back template

--- src/components/map.vue
+++ src/components/map.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="vue-map-container">
+  <div class="vue-map-container" :class="$attrs.class">
+    <div ref="vue-map" class="vue-map" :style="$attrs.style ? $attrs.style : ''"></div>
+    <div class="vue-map-hidden">
+      <slot></slot>
+    </div>
+    <slot name="visible"></slot>
   </div>
 </template>
 
